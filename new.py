f38d9d5282a675415d79ce317c2019d2c7181f56
kitso20 <motlashupingkitso@gmail.com>
2026-02-24 20:34:53 +0200

    temperature

--- new.py
+++ new.py
@@ -2,3 +2,14 @@ def greet_user(name, age):
     if age < 0:
         return "Invalid age"
     return f"Hello {name}, you are {age} years old."
+
+def classify_temperature(celsius):
+    if celsius < 0:
+        return "Freezing"
+    elif celsius <= 20:
+        return "Chilly"
+    elif celsius <= 35:
+        return "Warm"
+    else:
+        return "Hot"
+    
